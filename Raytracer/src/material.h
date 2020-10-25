d02f72444df03d6c46eab795d33fd9e96d83ef5c
Arkadiusz Wiśniewski <syadnom.etah@gmail.com>
2020-10-25 20:10:21 +0100

    Implemented dielectric material

--- Raytracer/src/material.h
+++ Raytracer/src/material.h
@@ -29,3 +29,22 @@ public:
 	double fuzz;
 };
 
+class dielectric : public material
+{
+public:
+	dielectric(double index_of_refraction);
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override;
+
+public:
+	double ir;
+
+private:
+	static double reflectance(double cosine, double ref_idx)
+	{
+		auto r0 = (1 - ref_idx) / (1 + ref_idx);
+		r0 = r0 * r0;
+
+		return r0 + (1 - r0) * pow((1 - cosine), 5);
+	}
+};
+
